015e437922884389cdeb3868c80d36f928972c82
Lirimerie <130313245+Lirimerie@users.noreply.github.com>
2023-10-07 02:04:23 +0200

    Forgot about the free throws

--- Main_Wrangle.R
+++ Main_Wrangle.R
@@ -1,15 +1,15 @@
-euroleague<-read.zip("data/euroleague.zip")
+euroleague<-read.csv("data/euroleague.csv")
 library(tidyverse)
 
-#unique_playtypes <- unique(euroleague$PLAYINFO)
-#unique_playtypes
+unique_playtypes <- unique(euroleague$PLAYINFO)
+unique_playtypes
 
 
 euroleague <- euroleague |>
   mutate(PLAYINFO = str_remove(PLAYINFO, "\\s*\\([^)]+\\)"))
 
-# Ugly line to reduce the details of the Play information to 33 different ones
-# by removing a space and what is inside the parenthesis
+#Ugly line to reduce the details of the Play information to 33 different ones
+#by removing a space and what is inside the parenthesis
 
 
 
@@ -17,22 +17,24 @@ unique_playtypes <- unique(euroleague$PLAYINFO)
 unique_playtypes
 
 euroleague <- euroleague |>
-  filter(!str_detect(PLAYINFO, "Out|In|Fighting|
-                     Bench Foul|Coach Foul|Begin Period|End Period"))|>
-  select(-TYPE,-COMMENT,-PLAYTYPE,-CODETEAM, -DORSAL,-MARKERTIME,-PLAYER_ID,
+  filter(!str_detect(PLAYINFO, "Fighting|Bench Foul|Coach Foul|Begin Period|
+                     End Period"))|>
+  select(-TYPE,-PLAYTYPE,-CODETEAM, -DORSAL,-MARKERTIME,-PLAYER_ID,
          -X,-NUMBEROFPLAY) 
 
 # all variables in select() are unnecessary variables. 
-# Out|In could be interesting to measure time on the court
-# I dont know which story could be told with Fighting
+# Out|In could be taken out but we need to let Free throw in in the data set
+#(Will work on it)
+# I don't know which story could be told with Fighting
 # Begin Period|End Period are not that necessary or so do I think as of now
 # Coach Foul and Bench Foul are not as relevant
 
-#euroleague <- euroleague |>
-#  mutate(points_made = case_when(
-#    PLAYINFO %in% c("Layup made", "Two Pointer","Dunk") ~ 2,
-#    PLAYINFO == "Three Pointer" ~ 3,
-#    TRUE ~ 0
-#  ))
+euroleague <- euroleague |>
+  mutate(points_made = case_when(
+    PLAYINFO %in% c("Layup made", "Two Pointer","Dunk") ~ 2,
+    PLAYINFO == "Three Pointer" ~ 3,
+    PLAYINFO == "Free Throw In" ~ 1,
+    TRUE ~ 0
+  ))
 
 
