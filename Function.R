a220349f5be891195549c086691db1da2616c4f6
dylsrm <dylanreismatos@gmail.com>
2023-10-11 17:17:56 +0200

    nouveau graph

--- Function.R
+++ Function.R
@@ -1,21 +1,27 @@
-generate_points_plot <- function(year) {
-  df <- team_stats_season %>% filter(year == year)
+create_team_points_plot <- function(Year) {
+  df <- team_stats_season %>% filter(year == Year)
   df_sorted <- df %>% arrange(desc(average_points))
   top_teams <- head(df_sorted, 5)
   bottom_teams <- tail(df_sorted, 5)
   combined_teams <- rbind(top_teams, bottom_teams)
   
-  points_plot <- ggplot(combined_teams, aes(x = reorder(Team, -average_points), y = average_points, fill = ifelse(rank(average_points) <= 5, "Bottom 5", "Top 5"))) +
+  # Tri des combined_teams
+  combined_teams <- combined_teams %>% arrange(desc(average_points))
+  
+  plot <- ggplot(combined_teams, 
+                 aes(x = reorder(Team, -average_points),
+                     y = average_points, 
+                     fill = ifelse(rank(average_points) <= 5, "Bottom 5", "Top 5"))) +
     geom_bar(stat = "identity") +
-    scale_fill_manual(values = c("Top 5" = "green", "Bottom 5" = "red")) +  
-    labs(title = paste("Top 5 and Bottom 5 Teams in", year, "by Average Points"),
+    scale_fill_manual(values = c("Top 5" = "green", "Bottom 5" = "red")) +
+    labs(title = paste("Top 5 and Bottom 5 Teams in", Year, "by Average Points"),
          x = "Team",
          y = "Average Points",
          fill = "") +
     theme(axis.text.x = element_text(angle = 40, hjust = 1)
     )
   
-  return(points_plot)
+  return(plot)
 }
 team_stats_plot_over_years <- function(team_stats_season, variable, title, y_label, p1_name) {
   
